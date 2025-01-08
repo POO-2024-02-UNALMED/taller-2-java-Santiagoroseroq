ce3d7d6b3c2a9ee5c6117c988074f98f9e444fd6
Santiagoroseroq <sroseroq@unal.edu.co>
2025-01-08 18:14:01 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -4,7 +4,7 @@ public class Asiento {
 	int precio;
 	int registro;
 	
-	void camabiaColor(String color) {
+	void cambiarColor(String color) {
 		if (color == "verde" || color == "amarillo" || color == "blanco" || color == "negro" || color == "rojo") {
 			this.color = color;
 		}
